edf7d3953e91b68efee0644e2aaf7d4602351179
Paul John <rainson.work@gmail.com>
2024-01-12 23:05:42 +0300

    Array length method

---
+++ cs/0-learncs/ArrayLength/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLengthApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            int length = array.Length;
+            Console.WriteLine("Length of array: {0}", length);
+
+            long longLength = array.LongLength;
+            Console.WriteLine("Length of the LongLength array: {0}", longLength);
+
+            int[,] twoD = new int[5, 10];
+            Console.WriteLine("The size of the 2D array is: {0}", twoD.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
