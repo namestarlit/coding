a11a0af7d702afd9a3ad0caa0d4b6f7bd6e3cfb2
Paul John <rainson.work@gmail.com>
2024-01-21 19:53:00 +0300

    learnCsharp.cs

--- cs/0-learncs/learnCsharp.cs
+++ cs/0-learncs/learnCsharp.cs
@@ -1,1 +1,446 @@
+// Single-line comments start with //
+
+/*
+ * Multi-line comments look like this
+ */
+
+/// <summary>
+/// This is an XML documentation comment which can be used to generate
+/// external docmentation or provide context help within an IDE
+/// </summary>
+/// <param name="firstParam">This is some parameter documentation for
+/// firstParam</param>
+/// <returns>Information on the returned value of a function</returns>
+public void MethodOrClassOrOtherWithParsableHelp(string firstParam) {}
+
+// Specify the namespaces this source code will be using
+// The namespaces below are all part of the standard .NET Framework
+// Class Library
 using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.IO;
+
+// But this one is not:
+using System.Data.Entity;
+// In order to be able to use it, you need to add a ddl reference
+// This can be done with the NuGet package manager:
+// `Install-Package EntityFramework`
+
+// Namespaces define scope to organize code into "packages" or "modules"
+// Using this code from another source file: using Learning.Csharp;
+namespace Learning.Csharp
+{
+    // Each .cs file shold at least contain a class with the same name
+    // as the file.
+    // You are allowed to do otherwise, but sholdn't for sanity.
+    public class LearningCsharp
+    {
+        // BASIC SYNTAX - skip to INTERESTING FEATURES if you have used
+        // Java or C++ before
+        public static void Syntax()
+        {
+            // Use Console.WriteLine to print lines
+            Console.WriteLine("Hello World");
+            Console.WriteLine(
+                    "Integer: " + 10
+                    + "Double: " + 3.14
+                    + "Boolean: " + true
+                    );
+
+            // To print without a new line, use Console.Write
+            Console.Write("Hello ");
+            Console.Write("World");
+
+            /////////////////////////////////////////////////
+            // Types ¥ Variables
+            // 
+            // Declare a variable sing <type> <name>
+            /////////////////////////////////////////////////
+
+            // Sbyte - Signed 8-bit integer
+            // (-128 <= sbyte <= 127)
+            sbyte fooSbyte = 100;
+
+            // Byte - Unsigned 8-bit integer
+            // (0 <= byte <= 255)
+            byte fooByte = 100;
+
+            // Short - 16-bit integer
+            // Signed - (-32,768 <= short <= 32,767)
+            // Unsigned - (0 <= ushort <= 65,535)
+            short fooShort = 10000;
+            ushort fooUshort = 10000;
+
+            // Integer - 32-bit integer
+            int fooInt = 1; // (-2,147,483,648 <= int <= 2,147,483,647)
+            uint fooUint = 1; // (0 <= uint <= 4,294,967,295)
+
+            // Long - 64-bit integer
+            long fooLong = 100000L; // (-9,223,372,036,854,775,808 <= long <= 9,223,372,036,854,775,807)
+            ulong fooUlong = 100000L; // (0 <= ulong <= 18,446,744,073,709,551,615)
+            // Numbers default to being int or uint depending on size.
+            // L is used to denote that this variable value is of type long or ulong
+            
+            // Double - Double-precision 64-bit IEEE 754 Floating Point
+            double fooDouble = 123.4; // Precision: 15-16 digits
+
+            // Float - Single-precision 32-bit IEEE 754 Floating Point
+            float fooFloat = 234.5f; // Precision: 7 digits
+            // f is used to denote that this variable is of type float
+
+            // Decimal - a 128-bits data type, with more precision
+            // than other floating-point types
+            decimal fooDecimal = 150.3m;
+
+            // Boolean - true & false
+            bool fooBoolean = true; // or false
+
+            // Char - A single 16-bit Unicode character
+            char fooChar = 'A';
+
+            // Strings -- unlike the previous base types which are all
+            // value types, a string is a reference type.
+            // That is you can set it to a null
+            string fooString = "\"escape\" quotes and add \n (new lines) and \t (tabs)";
+            Console.WriteLine(fooString);
+
+            // You can access each character of the string with an indexer
+            char charFromString = fooString[1]; // => 'e'
+            // Strings are immutable: you can't do fooString[1] = 'X';
+            
+            // Compare strings with current culture, ignoreing case
+            string.Compare(fooString, "x", stringComparison.CurrentCultureIgnoreCase);
+
+            // Formatting, based on the sprintf
+            string fooFs = string.Format("Check Check, {0} {1}, {0} {1:0.0}", 1, 2);
+
+            // Dates & Formatting
+            DateTime fooDate = DateTime.Now;
+            Console.WriteLine(fooDate.ToString("hh:mm, dd MMM yyyy"));
+
+            // Verbatim String
+            // You can use the @ symbol before a string literal to escape
+            // characters in the string
+            string path = "/tmp";
+            string verbatimPath = @"/tmp";
+            Console.WriteLine(path == verbatimPath); // => true
+
+            // You can split a string over two lines with the @ symbol.
+            // To escape " use ""
+            string bazString = @"Here's some stuff
+                on a new line! ""Wow!"", the masses cried";
+
+            // use const or read-only to make a variable immutable
+            // const values are calculated at compile time
+            const int HoursWorkPerWeek = 9001;
+
+            /////////////////////////////////////////////////
+            // Data Structures
+            /////////////////////////////////////////////////
+
+            // Arrays - zero indexed 
+            // The array size must be decided upon declaration
+            // The format for declaring an array is
+            // <datatype[] <varName> new <datatype>[<array size>];
+            int[] intArray = new int[10];
+
+            // Another way to declare and initialize an array
+            int[] y = { 9000, 1000, 1337 };
+
+            // Indexing an array - Accessing an element
+            Console.WriteLine("intArray @ 0: " + intArray[0]);
+            // Arrays are mutable
+            intArray[1] = 1;
+
+            // Lists
+            // Lists are used more frequently than arrays as they are more flexible
+            // The format for declaring a list is
+            // List<datatype> <varName> = new List<datatype>();
+            List<int> intList = new List<int>();
+            List<string> stringList = new List<string>();
+            List<int> z = new List<int> { 9000, 1000, 1337 }; // initialize
+            // The <> are for generics - Checkot the cool stuff section
+            
+            // Lists don't default to a value
+            // A value must be added before accessing the index
+            intList.Add(1);
+            Console.WriteLine("intList at 0: " + intList[0]);
+
+            // Other data structures to check out:
+            // Stack/Queue
+            // Dictionary (an implementation of a hash map)
+            // HashSet
+            // Read-only collections
+            // Tuple (.NET 4+)
+
+            /////////////////////////////////////////////////
+            // Operators
+            /////////////////////////////////////////////////
+            Console.WriteLine("\n-> Operators");
+            int  i1 = 1, i2 = 2; // Shorthand for multiple declarations
+
+            // Arithmetic is straightforward
+            Console.WriteLine(i1 + i2 - i1 * 3 / 7); // => 3
+
+            // Modulo
+            Console.WriteLine("11 % 3 = ", + (11 % 3)); // => 2
+
+            // Comparison operators
+            Console.WriteLine("3 == 2? " + (3 == 2)); // => false
+            Console.WriteLine("3 != 2? " + (3 != 2)); // => true
+            Console.WriteLine("3 > 2? " + (3 > 2)); // => true
+            Console.WriteLine("3 < 2? " + (3 < 2)); // => false
+            Console.WriteLine("2 <= 2? " + (2 <= 2)); // true
+            Console.WriteLine("2 >= 2?" + (2 >= 2)); // => true
+
+            // Bitwise Operators!
+            /*
+             * ~    Unary bitwise complement
+             * <<   Signed left shift
+             * >>   Signed right shift
+             * &    Bitwise AND
+             * ^    Bitwise exclusive OR (XOR)
+             * |    Bitwise inclusive OR
+             */
+
+            // Incrementing
+            int i = 0;
+            Console.WriteLine("->Inc/Dec-rement");
+            Console.WriteLine(i++); // Prints "0", i = 1. Post-Increment
+            Console.WriteLine(++i); // Prints "2", i = 2. Pre-Increment
+            Console.WriteLine(i--); // Prints "2", i = 1. Post-Decrement
+            Console.WriteLine(--i); // Prints "0", i = 0. Pre-Decrement
+
+            /////////////////////////////////////////////////
+            // Control Structures
+            /////////////////////////////////////////////////
+            Console.WriteLine("\n->Control Structures");
+
+            // If statements are C-like
+            int j = 10;
+            if (j == 10)
+            {
+                Console.WriteLine("I get printed");
+            }
+            else if ( j > 10)
+            {
+                Console.WriteLine("I don't");
+            }
+            else
+            {
+                Console.WriteLine("I also don't");
+            }
+
+            // Ternary operators
+            // A simple if/else can be written as follows
+            // <condition> ? <true> : <false>
+            int toCompare = 17;
+            string isTrue = toCompare == 17 ? "True" : "False";
+
+            // While loop
+            int fooWhile = 0;
+            while (fooWhile < 100)
+            {
+                // Iterated 100 times, fooWhile 0 -> 99
+                fooWhile++;
+            }
+
+            // Do While Loop
+            int fooDoWhile = 0;
+            do
+            {
+                // Start iteration 100 times, fooDoWhile 0 -> 99
+                if (false)
+                    continue; // skip the current iteration
+                
+                fooDoWhile++;
+
+                if (fooDoWhile == 50)
+                    break; // breaks from the loop completely
+            } while (fooDoWhile < 100);
+
+            // for loop structure => for (<start_statement>; <codintional; <step>)
+            for (int fooFor = 0; fooFor < 10; fooFor++)
+            {
+                //Iterated 10 times, fooFor 0 -> 9
+            }
+
+            // For Each Loop
+            // foreach loop structure:
+            // foreach(<iteratorType> <iteratorName> in <enumerable>)
+            // The foreach loop loops over any object implementing
+            // IEnumerable or IEnumerable<T>
+            // All the collection types (Array, List, Dictionary...)
+            // in the .NET Framework implement one or both of these interfaces
+            // (The ToCharArray() could be removed, because a string
+            // implements IEnumerable)
+            foreach (char character in "Hello World".ToCharArray())
+            {
+                // Iterated over all the characters in the string
+            }
+
+            // Switch Case
+            // A switch works with byte, short, char, and int data types.
+            // it also works with enumerated types (discussed in Enum Types)
+            // the String class, and a few special classes that wrap
+            // primitive types: Character, Byte, Short, and Integer
+            int month = 3;
+            string monthString;
+            switch (month)
+            {
+                case 1:
+                    monthString = "January";
+                    break;
+                case 2:
+                    monthString = "February";
+                    break;
+                case 3:
+                    monthString = "March";
+                    break;
+                    // You can assign more than one case to an action
+                    // But you can't add an action without a break before
+                    // another case
+                    // (if you want to do this, you would have to
+                    // explicitily add a goto case x)
+                case 6:
+                case 7:
+                case 8:
+                    monthString = "Summer time!!";
+                    break;
+                default:
+                    monthString = "Some other month";
+                    break;
+            }
+
+            /////////////////////////////////////////////////
+            // Converting Data Types And Typecasting
+            /////////////////////////////////////////////////
+
+            // Converting data
+
+            // Convert String To Integer
+            // this will throw a FormatException on failure
+            int.Parse("123"); // returns an integer version of "123"
+
+            // TryParse will default to the type's default value on
+            // failure, in this case 0
+            int tyrInt;
+            if (int.TryParse("123", out tryInt)) // Function is boolean
+                Console.WriteLine(tryInt);  // 123
+
+            // Convert Integer To String
+            // The Convert class has a number of methods to facilitate
+            // conversations String to int
+            bool result = int.TryParse(string, out var integer);
+            int.Parse(string);
+
+            // Not recommended
+            Convert.ToString(123);
+
+            // Int to String
+            tryInt.ToString();
+
+            // Casting
+            // Cast decimal 15 to an int
+            // and then implicitly cast to long
+            long x = (int) 15M;
+        }
+
+        /////////////////////////////////////////////////////
+        // CLASSES - see definitions at end of file
+        /////////////////////////////////////////////////////
+        public static void Classes()
+        {
+            // See Declarations of objects at end of file
+
+            // Use new to instantiate a class
+            Bicycle trek = new Bicycle();
+
+            // Call object methods
+            trek.SpeedUp(3); // You should always use setter and getter methods
+            trek.Cadence = 100;
+
+            // ToString is a conversion to display the value of this object
+            Console.WriteLine("trek info: " + trek.Info());
+
+            // Instantiate a new Penny Farthing
+            PennyFarthing funbike = new PennyFarthing(1, 10);
+            Console.WriteLine("funbike info: " + funbike.Info());
+
+            Console.Read();
+        } // End main method
+
+        // Available in C# 9 and later, this is basically syntantic
+        // sugar for a class. Records are immutable
+        public record ARecord(string CSharp);
+
+        // CONSOLE ENTRY - A Console application must have a main method
+        // as an entry point
+        public static void Main(string[] args)
+        {
+            OtherInterestingFeatures();
+        }
+
+        //
+        //INTERESTRING FEATURES
+        //
+
+        // DEFAULT METHOD SIGNATURES
+
+        public // Visibility
+        static // Allows for direct call on class without object
+        int // Return Type,
+        MethodSignatures(
+                int maxCount, // First variable, expects an int
+                int count = 0, // will default the value to 0 if not passed in
+                int another = 3,
+                params string[] otherParams // captures all other parameters passed
+                )
+        {
+            return -1;
+        }
+
+        // Methods can have same name, as long as the signature is unique
+        // A method that differs only in return type is not unique
+        public static void MethodSignatures(
+                ref int maxCount, // Pass by reference
+                out int count
+                )
+        {
+            // the argument passed in as 'count' will hold the value
+            // of 15 outside of the function
+            count = 15; // out param must be assigned before control leaves the method
+        }
+
+        // GENERICS
+        // The classes for TKey and Tvalue is specified by
+        // the user calling this function.
+        // This method emulates Python's dict.setdefault()
+        public static TValue SetDefault<Tkey, Tvalue>(
+                IDictinary<Tkey, Tvalue> dictionary,
+                TKey key,
+                Tvalue deafultItem
+                )
+        {
+            Tvalue result;
+            if (!dictionary.TryGetValue(key, out result))
+                return dictionary[key] = defaultItem;
+            return result;
+        }
+
+        // You can narrow down the objects that are passed in
+        public static void IterateAndPrint<T>(T toPrint) where T: IEnumerable<int>
+        {
+            // We can iterate, since T is an IEnumerable
+            foreach (var item in toPrint)
+                // Item is an int
+                Console.WriteLine(item.ToString());
+        }
+
+        // YIELD
+    }
+}
