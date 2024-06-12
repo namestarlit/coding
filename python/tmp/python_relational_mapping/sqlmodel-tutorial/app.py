5a5340ac9d480e27d224886dd99efb92e02fd41f
Paul John <rainson.work@gmail.com>
2024-06-12 11:51:31 +0300

    feat: create rows in database

--- python/tmp/python_relational_mapping/sqlmodel-tutorial/app.py
+++ python/tmp/python_relational_mapping/sqlmodel-tutorial/app.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, Session, SQLModel, create_engine
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
 class Hero(SQLModel, table=True):
@@ -19,24 +19,29 @@ def create_db_and_tables():
 
 def create_session():
     session = Session(engine)
-    return session 
+    return session
 
-def main():
-    create_db_and_tables()
-    
+def create_heroes():
     hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
     hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Paraqueador")
     hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
 
-    session = create_session()
+    with Session(engine) as session:
+        session.add(hero_1)
+        session.add(hero_2)
+        session.add(hero_3)
 
-    session.add(hero_1)
-    session.add(hero_2)
-    session.add(hero_3)
+        session.commit()
 
-    session.commit()
+def select_heroes():
+    with Session(engine) as session:
+        statement = select(Hero)
 
-    session.close()
+
+
+def main():
+    create_db_and_tables()
+    create_heroes()
 
 
 if __name__ == "__main__":
