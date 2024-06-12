5d860f96b8e145b55c049fbdfb942a6e15d8aabf
Paul John <rainson.work@gmail.com>
2024-06-12 12:43:01 +0300

    feat: filter data

--- python/tmp/python_relational_mapping/sqlmodel-tutorial/app.py
+++ python/tmp/python_relational_mapping/sqlmodel-tutorial/app.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, or_, col, select
 
 
 class Hero(SQLModel, table=True):
@@ -8,42 +8,50 @@ class Hero(SQLModel, table=True):
     age: int | None = None
 
 
-# create engine 
+# create engine
 sqlite_db = "database.db"
 sqlite_url = f"sqlite:///{sqlite_db}"
 
 engine = create_engine(sqlite_url, echo=True)
 
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
 
 def create_session():
     session = Session(engine)
     return session
 
+
 def create_heroes():
     hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
     hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Paraqueador")
     hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+    hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+    hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
+    hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+    hero_7 = Hero(name="Captain North America", secret_name="Esteban Rogelios", age=93)
 
     with Session(engine) as session:
         session.add(hero_1)
         session.add(hero_2)
         session.add(hero_3)
+        session.add(hero_4)
+        session.add(hero_5)
+        session.add(hero_6)
+        session.add(hero_7)
 
         session.commit()
 
+
 def select_heroes():
     with Session(engine) as session:
-        statement = select(Hero)
-
-        heroes = session.exec(statement).all()
-        print(heroes)
+        statement = select(Hero).where(or_(col(Hero.age) >= 35, col(Hero.age) < 40))
 
         results = session.exec(statement)
         for hero in results:
             print(hero)
-
 
 
 def main():
